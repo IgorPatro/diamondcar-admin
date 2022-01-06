9a0fcefabf409c22faf9b611943055e872633328
Igor Patro <tools@skyagency360.pl>
2022-01-06 14:45:54 +0100

    Created basic button

---
+++ src/UserForm.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const UserForm = () => {
+  return (
+    <button
+      type="button"
+      className="py-4 px-5 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 text-white transition ease-in duration-200 text-center text-5xl font-semibold shadow-md rounded-full"
+    >
+      <span className="-mt-3">+</span>
+    </button>
+  )
+}
+
+export default UserForm
